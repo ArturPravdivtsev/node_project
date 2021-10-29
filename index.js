50da1763376b14aebf76f831e9d28ed2be8e81d1
Артур Правдивцев <artur@MacBook-botmother.local>
2021-10-29 17:01:09 +0300

    added http file server

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+const http = require('http');
+const path = require('path');
 const fs = require('fs');
-const ACCESS_LOG = './access.log';
 
-const readStream = fs.createReadStream(ACCESS_LOG, 'utf-8');
+(async () => {
+    const isFile = (filePath) => fs.lstatSync(filePath).isFile();
 
-readStream.on('data', (chunk) => {
-    const lines = chunk.split('\n');
-    lines.forEach(line => {
-        const ip = line.split(' ')[0];
-        if (ip === '89.123.1.41' || ip === '34.48.240.111') {
-            fs.appendFile(`./${ip}_requests.log`,
-                `${line}\n`,
-                {
-                    encoding: 'utf-8',
-                },
-                (err) => {
-                    if (err) console.log(err);
-                }
-            );
+    http.createServer((req, res) => {
+        const fullPath = path.join(process.cwd(), req.url);
+
+        if (!fs.existsSync(fullPath)) return res.end('File or directory not found');
+
+        if (isFile(fullPath)) return fs.createReadStream(fullPath).pipe(res);
+
+        let linksList = '';
+
+        const urlParams = req.url.match(/[\d\w\.]+/gi);
+
+        if (urlParams) {
+            urlParams.pop();
+            const prevUrl = urlParams.join('/');
+
+            linksList = urlParams.length ? `<li><a href="/${prevUrl}">...</a></li>` : `<li><a href="/">...</a></li>`;
         }
-    });
-});
 
-readStream.on('end', () => console.log('Finished!'));
+        fs.readdirSync(fullPath)
+            .forEach(fileName => {
+                const filePath = path.join(req.url, fileName);
+                linksList += `<li><a href="${filePath}">${fileName}</a></li>`;
+            });
+
+        const HTML = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8')
+            .replace('##links##', linksList);
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+        })
+        return res.end(HTML)
+    }).listen(8888);
+})();
